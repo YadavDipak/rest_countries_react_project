1f8f10df1b6b586698e648b138f0435e15e281be
YadavDipak <yadavdipak2911@gmail.com>
2025-01-20 12:10:26 +0530

    App javascript file

---
+++ restCountries/src/App.jsx
@@ -0,0 +1,22 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import NotFoundPage from "./pages/NotFoundPage";
+import DetailPage from "./pages/DetailsPage"
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+  const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+    <Route path="/country/:id" element={<DetailPage />} />
+    <Route path="*" element={<NotFoundPage />} />
+  </Route>))
+  return (
+    <RouterProvider router={router} />
+  )
+};
+export default App;
