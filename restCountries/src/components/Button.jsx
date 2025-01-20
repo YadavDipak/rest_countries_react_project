2b7ab9f0eefc9aa740f1ea2c4caf87663ea52c57
YadavDipak <yadavdipak2911@gmail.com>
2025-01-20 12:13:03 +0530

    Button file managed country and region

---
+++ restCountries/src/components/Button.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useTheme } from "../context/ThemeContext";
+import { useData } from "../context/DataContext";
+
+const Button = ({ children, classes, link }) => {
+    const { setCountryID, setCountryName, setRegion } =
+        useData();
+    const { theme } = useTheme();
+    // handleClick function
+    const handleClick = () => {
+        setCountryID(null);
+        setCountryName(null);
+        setRegion(null);
+    };
+    return (
+        <Link
+            to={link}
+            onClick={handleClick}
+            className={` ${theme.subBackground} ${theme.textColor} ${classes} hover:bg-gray-400 hover:text-black rounded-md py-2 mt-4 shadow-lg font-semibold flex`}
+        >
+            {children}
+        </Link>
+    );
+};
+export default Button;
