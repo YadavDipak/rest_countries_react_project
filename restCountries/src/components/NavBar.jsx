d2af8ee86a1bd1ff5623a3876fd44cca37225902
YadavDipak <yadavdipak2911@gmail.com>
2025-01-20 13:04:36 +0530

    NavBar updated

--- restCountries/src/components/NavBar.jsx
+++ restCountries/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { IoMoonOutline } from "react-icons/io5";
 import { useTheme } from "../context/ThemeContext";
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     const { theme, toggleMode } = useTheme();
@@ -10,9 +11,12 @@ const NavBar = () => {
                 className={`flex justify-between ${theme.subBackground} ${theme.textColor} md:px-20 px-10 py-6 shadow-sm`}
                 key="navbar-section"
             >
-                <h1 className="text-xl font-bold md:text-2xl">
-                    Where in the world?
-                </h1>
+                <Link
+                    to="/">
+                    <h1 className="text-xl font-bold md:text-2xl">
+                        Where in the world?
+                    </h1>
+                </Link>
                 <button type="button" className="text-lg" onClick={toggleMode}>
                     <IoMoonOutline className="inline mb-1 mr-1" /> Dark Mode
                 </button>
