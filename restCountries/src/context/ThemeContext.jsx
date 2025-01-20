9be05df94632ab9474aa6d02f85a922a42795615
YadavDipak <yadavdipak2911@gmail.com>
2025-01-20 12:17:30 +0530

    ThemeContext file added

---
+++ restCountries/src/context/ThemeContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState, useContext } from "react";
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState({ mainBackground: "bg-veryLightGray", textColor: "text-veryDarkBlueText", subBackground: "bg-dark_blue_dm" });
+    const [darkMode, setMode] = useState(true);
+
+    const toggleMode = () => {
+        if (darkMode) {
+            setMode(false);
+            setTheme({
+                textColor: "text-white",
+                mainBackground: "bg-veryDarkBlueBg",
+                subBackground: "bg-darkBlue"
+            })
+        } else {
+            setMode(true);
+            setTheme({ mainBackground: "bg-veryLightGray", textColor: "text-veryDarkBlueText", subBackground: "bg-dark_blue_dm" })
+        }
+    };
+    return (
+        <ThemeContext.Provider value={{ theme, toggleMode }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+export const useTheme = () => useContext(ThemeContext);
+
+
+
+
