acd6d1ef5b2ff6fe4ff2201a562388a9690f075d
YadavDipak <yadavdipak2911@gmail.com>
2025-01-29 23:12:38 +0530

    Filter file

--- restCountries/src/components/FilterByCountry.jsx
+++ restCountries/src/components/FilterByCountry.jsx
@@ -1,22 +1,23 @@
-import React from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 import { useTheme } from "../context/ThemeContext";
-import { FaMagnifyingGlass } from 'react-icons/fa6'
 
 const FilterByCountry = ({ searchHandler }) => {
-    const { theme } = useTheme()
-    return (
-        <div
-            className={`py-4 px-6 md:px-8 ${theme.subBackground} justify-self-start w-full xl:w-2/3 rounded-md md:col-span-2 shadow-lg flex`}
-        >
-            <FaMagnifyingGlass className="mr-2 mt-1" />
-            <input
-                className={`${theme.subBackground} focus:${theme.subBackground} outline-none border-none w-full`}
-                id="filter"
-                type="text"
-                placeholder="Search for a country..."
-                onChange={searchHandler}
-            />
-        </div>
-    );
+  const { theme } = useTheme();
+
+  return (
+    <div
+      className={`py-4 px-6 md:px-8 ${theme.subBackground} justify-self-start w-full xl:w-2/3 rounded-md md:col-span-2 shadow-lg flex`}
+    >
+      <FaMagnifyingGlass className="mr-2 mt-1" />
+      <input
+        className={`${theme.subBackground} focus:${theme.subBackground} outline-none border-none w-full`}
+        id="filter"
+        type="text"
+        placeholder="Search for a country..."
+        onChange={searchHandler}
+      />
+    </div>
+  );
 };
+
 export default FilterByCountry;
--- restCountries/src/components/FilterContainer.jsx
+++ restCountries/src/components/FilterContainer.jsx
@@ -1,25 +1,21 @@
-import React, { useMemo } from "react";
-import FilterByCountry from "./FilterByCountry"
 import Filter from "./Filter";
+import FilterByCountry from "./FilterByCountry";
 import { useData } from "../context/DataContext";
 
 const FilterContainer = () => {
-    const { handleSearchInput, regionArray, handleRegionChange, } = useData();
-    return (
-        <section
-            className="px-12 py-10 xl:py-10 xl:px-20 lg:px-12 grid grid-cols-1 lg:grid-cols-5 items-center gap-y-8 lg:gap-8"
-        >
-            {/* Search */}
-            <FilterByCountry searchHandler={handleSearchInput} />
-            {/* Filter Region */}
+  const { handleSearchInput, regionArray, handleRegionChange } = useData();
 
-            <Filter
-                title="Region"
-                filterHandler={handleRegionChange}
-                populateArray={regionArray}
-            />
+  return (
+    <section className="px-12 py-10 xl:py-10 xl:px-20 lg:px-12 grid grid-cols-1 lg:grid-cols-5 items-center gap-y-8 lg:gap-8">
+      <FilterByCountry searchHandler={handleSearchInput} />
 
-        </section>
-    );
+      <Filter
+        title="Region"
+        filterHandler={handleRegionChange}
+        populateArray={regionArray}
+      />
+    </section>
+  );
 };
+
 export default FilterContainer;
