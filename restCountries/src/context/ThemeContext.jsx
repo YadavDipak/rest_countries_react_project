51f6e27f6c3457282c57651ce1819bca9da4a96d
YadavDipak <yadavdipak2911@gmail.com>
2025-01-29 23:09:28 +0530

    Theme Context

--- restCountries/src/context/ThemeContext.jsx
+++ restCountries/src/context/ThemeContext.jsx
@@ -1,31 +1,39 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useState, useContext } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState({ mainBackground: "bg-veryLightGray", textColor: "text-veryDarkBlueText", subBackground: "bg-dark_blue_dm" });
-    const [darkMode, setMode] = useState(true);
+  const [theme, setTheme] = useState({
+    mainBackground: "bg-veryLightGray",
+    textColor: "text-veryDarkBlueText",
+    subBackground: "bg-dark_blue_dm",
+  });
 
-    const toggleMode = () => {
-        if (darkMode) {
-            setMode(false);
-            setTheme({
-                textColor: "text-white",
-                mainBackground: "bg-veryDarkBlueBg",
-                subBackground: "bg-darkBlue"
-            })
-        } else {
-            setMode(true);
-            setTheme({ mainBackground: "bg-veryLightGray", textColor: "text-veryDarkBlueText", subBackground: "bg-dark_blue_dm" })
-        }
-    };
-    return (
-        <ThemeContext.Provider value={{ theme, toggleMode }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  const [darkMode, setMode] = useState(true);
+
+  const toggleMode = () => {
+    if (darkMode) {
+      setMode(false);
+      setTheme({
+        textColor: "text-white",
+        mainBackground: "bg-veryDarkBlueBg",
+        subBackground: "bg-darkBlue",
+      });
+    } else {
+      setMode(true);
+      setTheme({
+        mainBackground: "bg-veryLightGray",
+        textColor: "text-veryDarkBlueText",
+        subBackground: "bg-dark_blue_dm",
+      });
+    }
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
+
 export const useTheme = () => useContext(ThemeContext);
-
-
-
-
