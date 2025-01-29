116fb6a68a45bb06e3c48786d970367f9dd48bfb
YadavDipak <yadavdipak2911@gmail.com>
2025-01-29 23:08:33 +0530

    Mainlayout file

--- restCountries/src/layout/MainLayout.jsx
+++ restCountries/src/layout/MainLayout.jsx
@@ -1,15 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import NavBar from '../components/NavBar';
-
-import React from 'react'
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 const MainLayout = () => {
-    return (
-        <>
-            <NavBar />
-            <Outlet />
-        </>
-    )
-}
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
