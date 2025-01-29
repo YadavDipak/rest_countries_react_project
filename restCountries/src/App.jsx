869d2c002efea529ca498e07435ee1c0ce9f0cab
YadavDipak <yadavdipak2911@gmail.com>
2025-01-29 23:05:35 +0530

    App JS file

--- restCountries/src/App.jsx
+++ restCountries/src/App.jsx
@@ -4,19 +4,23 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+
 import MainLayout from "./layout/MainLayout";
 import NotFoundPage from "./pages/NotFoundPage";
-import DetailPage from "./pages/DetailsPage"
+import DetailPage from "./pages/DetailsPage";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
-  const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<MainLayout />}>
-    <Route index element={<HomePage />} />
-    <Route path="/country/:id" element={<DetailPage />} />
-    <Route path="*" element={<NotFoundPage />} />
-  </Route>))
-  return (
-    <RouterProvider router={router} />
-  )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/country/:id" element={<DetailPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 };
 export default App;
