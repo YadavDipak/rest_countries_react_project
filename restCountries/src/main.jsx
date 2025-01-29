92ab9113b03879a272c19e8a9c23679d60df5cd6
YadavDipak <yadavdipak2911@gmail.com>
2025-01-29 23:05:55 +0530

    Main JS file

--- restCountries/src/main.jsx
+++ restCountries/src/main.jsx
@@ -1,17 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { DataProvider } from "./context/DataContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
       <DataProvider>
         <App />
       </DataProvider>
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
